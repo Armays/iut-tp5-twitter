ba8f467c85237a64e8a566fa07890ef57d42b3dc
Armays <diane.lakestani@gmail.com>
2017-06-21 10:30:42 +0200

    ajout du router-link timeline

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <p>
+        <router-link to="/timeline">Go to Timeline</router-link>
+    </p>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Timeline from '@/components/Timeline';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 export default {
   name: 'app'
 }
+
+const routes = [
+  { path: '/timeline', component: Timeline }
+]
+
+const router = new VueRouter({
+  routes // short for routes: routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
 </script>
 
 <style>
