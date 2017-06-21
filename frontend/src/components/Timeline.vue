49fe3ad92a50f953d0d6ad8c1331c8e403dc7e98
Armays <diane.lakestani@gmail.com>
2017-06-21 10:19:05 +0200

    nettoyage du front + ajout de la route timeline

---
+++ frontend/src/components/Timeline.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="timeline">
+  <ul>
+    <li v-for="tweet in tweets">{{tweet}}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+  data () {
+            return {
+                     tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+                   }
+          }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
