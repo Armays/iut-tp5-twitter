a183ca44d8c7969043388cfb2317d0c9d3f7a4f6
Armays <diane.lakestani@gmail.com>
2017-06-23 11:14:24 +0200

    étape 10: modification css des tweets

--- frontend/src/components/TweetComp.vue
+++ frontend/src/components/TweetComp.vue
@@ -1,16 +1,54 @@
 
 <template>
-  <div>{{ tweetProp.auteur.nom }} - {{tweetProp.auteur.prenom}} : {{tweetProp.contenu}}</div>
+  <div>
+    <div>
+      <strong>{{ tweetProp.auteur.nom }} - {{tweetProp.auteur.prenom}} : </strong>
+      <span class="handle">{{tweetProp.auteur.handle}}</span>
+    </div>
+    <div>
+      {{tweetProp.contenu}}
+    </div>
+    <div>
+      <ul>
+        <li class="button">
+          <icon name="reply"/>
+        </li>
+        <li class="button">
+          <icon name="retweet"/>
+        </li>
+        <li class="button">
+          <icon name="heart"/>
+        </li>
+        <li class="button">
+          <icon name="envelope"/>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
 export default {
   name: 'TweetComp',
-  props: ['tweetProp']
+  props: ['tweetProp'],
+  components: { Icon }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+li.button {
+ display: inline-block;
+}
+
+a {
+ color: #42b983;
+}
+
+span.handle {
+ color: gray;
+}
 </style>
